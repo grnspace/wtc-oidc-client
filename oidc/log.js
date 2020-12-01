0e851f92dfb43666558ef7e7c8f4d2c37a25b429
Alexander Draper <alex@greenspacehealth.com>
2020-12-01 14:54:14 -0800

    Update docs and make sample work

--- oidc/log.js
+++ oidc/log.js
@@ -1,13 +1,12 @@
 function log() {
-    document.getElementById('out').innerText = '';
+  document.getElementById('out').innerText = '';
 
-    Array.prototype.forEach.call(arguments, function(msg) {
-        if (msg instanceof Error){
-            msg = "Error: " + msg.message;
-        }
-        else if (typeof msg !== 'string') {
-            msg = JSON.stringify(msg, null, 2);
-        }
-        document.getElementById('out').innerHTML += msg + '\r\n';
-    });
+  Array.prototype.forEach.call(arguments, function (msg) {
+    if (msg instanceof Error) {
+      msg = 'Error: ' + msg.message;
+    } else if (typeof msg !== 'string') {
+      msg = JSON.stringify(msg, null, 2);
+    }
+    document.getElementById('out').innerHTML += msg + '\r\n';
+  });
 }
--- oidc/oidc-client-sample.js
+++ oidc/oidc-client-sample.js
@@ -4,8 +4,10 @@
 ///////////////////////////////
 // UI event handlers
 ///////////////////////////////
-document.getElementById('signin').addEventListener("click", signin, false);
-document.getElementById('processSignin').addEventListener("click", processSigninResponse, false);
+document.getElementById('signin').addEventListener('click', signin, false);
+document
+  .getElementById('processSignin')
+  .addEventListener('click', processSigninResponse, false);
 
 ///////////////////////////////
 // OidcClient config
@@ -13,20 +15,19 @@ document.getElementById('processSignin').addEventListener("click", processSignin
 Oidc.Log.logger = console;
 Oidc.Log.level = Oidc.Log.INFO;
 
-
 var staging = 'https://health-canada-test.grnspace.co/oauth2';
 var prod = 'https://ca.portal.gs/oauth2';
 
 var settings = {
-    authority: 'https://health-canada-test.grnspace.co/oauth2',
-    client_id: 'health-canada-demo-resource-pkce',
-    redirect_uri: 'https://grnspace.github.io/wtc-oidc-client/oidc/',
-    post_logout_redirect_uri: 'https://grnspace.github.io/wtc-oidc-client/oidc/',
-    response_type: 'code',
-    scope: 'openid email',
+  authority: 'https://health-canada-test.grnspace.co/oauth2',
+  client_id: 'health-canada-demo-resource-pkce',
+  redirect_uri: 'https://grnspace.github.io/wtc-oidc-client/oidc/',
+  post_logout_redirect_uri: 'https://grnspace.github.io/wtc-oidc-client/oidc/',
+  response_type: 'code',
+  scope: 'openid email',
 
-    filterProtocolClaims: true,
-    loadUserInfo: true
+  filterProtocolClaims: true,
+  loadUserInfo: true,
 };
 var client = new Oidc.OidcClient(settings);
 
@@ -34,27 +35,32 @@ var client = new Oidc.OidcClient(settings);
 // functions for UI elements
 ///////////////////////////////
 function signin() {
-    client.createSigninRequest({ state: { bar: 15 } }).then(function(req) {
-        log("signin request", req, "<a href='" + req.url + "'>go signin</a>");
-        window.location = req.url;
-    }).catch(function(err) {
-        log(err);
+  client
+    .createSigninRequest({ state: { bar: 15 } })
+    .then(function (req) {
+      log('signin request', req, "<a href='" + req.url + "'>go signin</a>");
+      window.location = req.url;
+    })
+    .catch(function (err) {
+      log(err);
     });
 }
 
 var signinResponse;
 function processSigninResponse() {
-    client.processSigninResponse().then(function(response) {
-        signinResponse = response;
-        log("signin response", signinResponse);
-    }).catch(function(err) {
-        log(err);
+  client
+    .processSigninResponse()
+    .then(function (response) {
+      signinResponse = response;
+      log('signin response', signinResponse);
+    })
+    .catch(function (err) {
+      log(err);
     });
 }
 
-if (window.location.href.indexOf("#") >= 0) {
-    processSigninResponse();
-}
-else if (window.location.href.indexOf("?") >= 0) {
-    processSignoutResponse();
+if (window.location.href.indexOf('#') >= 0) {
+  processSigninResponse();
+} else if (window.location.href.indexOf('?') >= 0) {
+  processSignoutResponse();
 }
