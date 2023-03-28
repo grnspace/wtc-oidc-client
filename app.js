3b5da41decac2363edea3174cc12a054fc0106f3
Eric Nelson <eric@greenspacehealth.com>
2023-03-28 11:51:45 -0700

    Make a test oidc for WTC that runs in the browser

--- app.js
+++ app.js
@@ -75,7 +75,7 @@
     );
 
     var host =
-      signInEl.dataset.host || 'https://app.wellnesstogether.grnspace.ca/';
+      signInEl.dataset.host || 'https://canada.grnspace.co:8000/';
     if (host[host.length - 1] !== '/') host += '/';
     fetch(host + 'oauth2/token', {
       method: 'POST',
